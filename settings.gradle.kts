5eaf3c3b11cf72ac5ca22eff295a5442f842d349
Kim woo jong <skekdnwhd@gmail.com>
2024-06-22 22:40:43 +0900

    Chore: change useModule to useVersion

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -14,8 +14,8 @@ pluginManagement {
     resolutionStrategy {
         eachPlugin {
             when (requested.id.id) {
-                "org.springframework.boot" -> useModule("org.springframework.boot:spring-boot-gradle-plugin:$springBootVersion")
-                "io.spring.dependency-management" -> useModule("io.spring.gradle:dependency-management-plugin:$springDependencyManagementVersion")
+                "org.springframework.boot" -> useVersion(springBootVersion)
+                "io.spring.dependency-management" -> useVersion(springDependencyManagementVersion)
             }
         }
     }
