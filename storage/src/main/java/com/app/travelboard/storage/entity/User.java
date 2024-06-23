e78da777a427b30617b50f6f2d27ffe4c79ed501
Kim woo jong <skekdnwhd@gmail.com>
2024-06-23 23:41:51 +0900

    Style: remove wildcard import

--- storage/src/main/java/com/app/travelboard/storage/entity/User.java
+++ storage/src/main/java/com/app/travelboard/storage/entity/User.java
@@ -1,7 +1,11 @@
 package com.app.travelboard.storage.entity;
 
-
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
