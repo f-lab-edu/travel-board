fd4c7c71aa612abfd7c1b8d73d65e1d8fb0ac299
Kim woo jong <skekdnwhd@gmail.com>
2024-06-29 18:09:55 +0900

    Chore: Add Spring Security dependency for password encoding of signup

--- api-user/build.gradle.kts
+++ api-user/build.gradle.kts
@@ -12,4 +12,7 @@ dependencies {
 
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+
+    testImplementation("org.springframework.security:spring-security-test")
 }
