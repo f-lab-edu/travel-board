058728f42ae41e1d1d7b8b341e0168af907d69be
Kim woo jong <skekdnwhd@gmail.com>
2024-06-30 16:20:18 +0900

    Fix: When registering as a user, answer 201 for a clear response value

--- api-user/src/main/java/com/user/controller/AuthController.java
+++ api-user/src/main/java/com/user/controller/AuthController.java
@@ -4,10 +4,13 @@ import com.user.controller.request.UserRegisterRequest;
 import com.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -17,9 +20,11 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public void signup(@RequestBody @Valid UserRegisterRequest request) {
+    public ResponseEntity<Void> signup(@RequestBody @Valid UserRegisterRequest request) {
         request.validate();
-        userService.register(request.toServiceRequest());
+        Long userId = userService.register(request.toServiceRequest());
+        URI location = URI.create(String.format("/users/%d", userId));
+        return ResponseEntity.created(location).build();
     }
 
 }
