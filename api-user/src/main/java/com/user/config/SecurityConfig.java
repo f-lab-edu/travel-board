3c3bafe8516d814594d5f19af5cb26b1b5918e18
kimwoojong <skekdnwhd@gmail.com>
2024-07-02 13:57:03 +0900

    Fix: remove @EnableWebSecurity and proxyBeanMethods false

--- api-user/src/main/java/com/user/config/SecurityConfig.java
+++ api-user/src/main/java/com/user/config/SecurityConfig.java
@@ -4,15 +4,13 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.argon2.Argon2PasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity
+@Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
 
     @Bean
