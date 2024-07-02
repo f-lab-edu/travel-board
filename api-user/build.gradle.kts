e53405bd425dd4730b55cf910fea42e7bcca7720
kimwoojong <skekdnwhd@gmail.com>
2024-07-02 10:10:39 +0900

    Chore: bouncycastle dependency 추가

--- api-user/build.gradle.kts
+++ api-user/build.gradle.kts
@@ -6,6 +6,7 @@ tasks.getByName("jar") {
     enabled = false
 }
 
+val bcprovVersion: String by project
 dependencies {
     implementation(project(":support"))
     implementation(project(":storage"))
@@ -13,6 +14,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.bouncycastle:bcprov-jdk18on:$bcprovVersion")
 
     testImplementation("org.springframework.security:spring-security-test")
 }
