30d0309ad5e09071b324b5ae7f496654a8b5eda4
Kim woo jong <skekdnwhd@gmail.com>
2024-07-03 09:13:12 +0900

    Fix: Delete refresh token related

--- api-user/src/main/java/com/user/service/RefreshTokenProvider.java
+++ api-user/src/main/java/com/user/service/RefreshTokenProvider.java
@@ -1,11 +1,7 @@
 package com.user.service;
 
-import com.storage.entity.Account;
-import com.storage.entity.RefreshToken;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
 
 @Component
 public class RefreshTokenProvider {
@@ -16,12 +12,4 @@ public class RefreshTokenProvider {
         this.validTimeInDays = validTimeInDays;
     }
 
-    public RefreshToken createToken(Account account) {
-        LocalDateTime expiredAt = LocalDateTime.now().plusDays(validTimeInDays);
-
-        return RefreshToken.builder()
-                .account(account)
-                .expiredAt(expiredAt)
-                .build();
-    }
 }
--- storage/src/main/java/com/storage/repository/RefreshTokenRepository.java
+++
@@ -1,7 +0,0 @@
-package com.storage.repository;
-
-import com.storage.entity.RefreshToken;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
-}
