91de762a4363de22ce7ff3865b9b3a8fad6d5966
Kim woo jong <skekdnwhd@gmail.com>
2024-07-03 09:13:12 +0900

    Fix: Delete refresh token related

--- api-user/src/main/java/com/user/controller/AuthController.java
+++ api-user/src/main/java/com/user/controller/AuthController.java
@@ -1,11 +1,9 @@
 package com.user.controller;
 
 import com.user.controller.request.UserRegisterRequest;
-import com.user.controller.response.UserRegisterResponse;
 import com.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseCookie;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -14,29 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
-import static org.springframework.http.HttpHeaders.SET_COOKIE;
-
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/auth")
 public class AuthController {
 
     private final UserService userService;
-    private final CookieProvider cookieProvider;
-    private static final String REFRESH_TOKEN_COOKIE_NAME = "REFRESH";
 
     @PostMapping("/signup")
     public ResponseEntity<Void> signup(@RequestBody @Valid UserRegisterRequest request) {
-        UserRegisterResponse response = userService.register(request);
-        ResponseCookie cookie = cookieProvider.createCookie(
-                REFRESH_TOKEN_COOKIE_NAME,
-                response.refreshTokenValue().toString(),
-                response.getMaxAgeForCookie()
-        );
-        URI location = URI.create(String.format("/users/%d", response.userId()));
-        return ResponseEntity.created(location)
-                .header(SET_COOKIE, cookie.toString())
-                .build();
+        Long userId = userService.register(request);
+        URI location = URI.create(String.format("/users/%d", userId));
+        return ResponseEntity.created(location).build();
     }
 
 }
--- api-user/src/main/java/com/user/service/UserService.java
+++ api-user/src/main/java/com/user/service/UserService.java
@@ -1,13 +1,10 @@
 package com.user.service;
 
 import com.storage.entity.Account;
-import com.storage.entity.RefreshToken;
 import com.storage.entity.User;
 import com.storage.repository.AccountRepository;
-import com.storage.repository.RefreshTokenRepository;
 import com.storage.repository.UserRepository;
 import com.user.controller.request.UserRegisterRequest;
-import com.user.controller.response.UserRegisterResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -22,25 +19,21 @@ public class UserService {
 
     private final AccountRepository accountRepository;
     private final UserRepository userRepository;
-    private final RefreshTokenRepository refreshTokenRepository;
-    private final RefreshTokenProvider refreshTokenProvider;
     private final PasswordEncoder passwordEncoder;
 
     @Transactional
-    public UserRegisterResponse register(UserRegisterRequest request) {
+    public Long register(UserRegisterRequest request) {
         accountRepository.findByEmail(request.email()).ifPresent(account -> {
             throw new IllegalStateException("Email is already in use");
         });
 
         String encodedPassword = passwordEncoder.encode(request.password());
         Account account = createAccount(request.email(), encodedPassword);
-        RefreshToken refreshToken = refreshTokenProvider.createToken(account);
         User user = createUser(account, request.nickname(), request.profileImageUrl(), request.bio());
 
         accountRepository.save(account);
-        refreshTokenRepository.save(refreshToken);
         userRepository.save(user);
-        return UserRegisterResponse.of(user.getId(), refreshToken.getTokenValue(), refreshToken.getExpiredAt());
+        return user.getId();
     }
 
     private Account createAccount(String email, String password) {
