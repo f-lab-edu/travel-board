f7cd636281de30c3559ca25f310891a8d27b9142
kimwoojong <skekdnwhd@gmail.com>
2024-07-09 13:48:01 +0900

    Feat : Manager is responsible for creating entity objects, add refreshTokenProvider

---
+++ api-user/src/main/java/com/user/domain/account/AccountManager.java
@@ -0,0 +1,14 @@
+package com.user.domain.account;
+
+import com.storage.entity.Account;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class AccountManager {
+
+    public static Account create(String email, String password, PasswordEncoder passwordEncoder) {
+        return Account.builder()
+                .email(email)
+                .password(passwordEncoder.encode(password))
+                .build();
+    }
+}
---
+++ api-user/src/main/java/com/user/domain/user/UserManager.java
@@ -0,0 +1,18 @@
+package com.user.domain.user;
+
+import com.storage.entity.Account;
+import com.storage.entity.User;
+
+import java.util.Optional;
+
+public class UserManager {
+
+    public static User create(Account account, String nickname, String profileImageUrl, String bio) {
+        return User.builder()
+                .account(account)
+                .nickname(nickname)
+                .profileImageUrl(Optional.ofNullable(profileImageUrl).orElse(""))
+                .bio(Optional.ofNullable(bio).orElse(""))
+                .build();
+    }
+}
