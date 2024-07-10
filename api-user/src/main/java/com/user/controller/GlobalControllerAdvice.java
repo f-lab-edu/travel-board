e4f5948b3ac062551af4e6059986a0256f8c236a
kimwoojong <skekdnwhd@gmail.com>
2024-07-10 17:27:03 +0900

    Refactor : Changed to enum static import, changed to responseStatus when giving status code.

--- api-user/src/main/java/com/user/controller/GlobalControllerAdvice.java
+++ api-user/src/main/java/com/user/controller/GlobalControllerAdvice.java
@@ -1,6 +1,5 @@
 package com.user.controller;
 
-import com.user.utils.error.ErrorType;
 import com.user.utils.error.TravelBoardException;
 import com.user.utils.response.ApiResponse;
 import lombok.extern.slf4j.Slf4j;
@@ -8,10 +7,16 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import static com.user.utils.error.ErrorType.DEFAULT_ERROR;
+import static com.user.utils.error.ErrorType.INVALID_REQUEST;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+import static org.springframework.http.HttpStatus.INTERNAL_SERVER_ERROR;
 
 @Slf4j
 @RestControllerAdvice
@@ -28,22 +33,22 @@ public class GlobalControllerAdvice {
                 .body(ApiResponse.error(e.getErrorType(), e.getData()));
     }
 
+    @ResponseStatus(BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<Void>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    public ApiResponse<Void> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         log.info("MethodArgumentNotValidException : {}", e.getMessage(), e);
         Map<String, String> validationData = new HashMap<>();
         for (FieldError fieldError : e.getFieldErrors()) {
             validationData.putIfAbsent(fieldError.getField(), fieldError.getDefaultMessage());
         }
-        return ResponseEntity.status(ErrorType.INVALID_REQUEST.getStatus())
-                .body(ApiResponse.error(ErrorType.INVALID_REQUEST, validationData));
+        return ApiResponse.error(INVALID_REQUEST, validationData);
     }
 
+    @ResponseStatus(INTERNAL_SERVER_ERROR)
     @ExceptionHandler(Exception.class)
-    public ResponseEntity<ApiResponse<?>> handleException(Exception e) {
+    public ApiResponse<Void> handleException(Exception e) {
         log.error("Exception : {}", e.getMessage(), e);
-        return ResponseEntity.status(ErrorType.DEFAULT_ERROR.getStatus())
-                .body(ApiResponse.error(ErrorType.DEFAULT_ERROR));
+        return ApiResponse.error(DEFAULT_ERROR);
     }
 
 }
