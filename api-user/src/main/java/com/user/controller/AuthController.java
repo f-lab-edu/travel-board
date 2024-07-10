6a93266c3869193ff8a35658e09b8daab531d3dc
kimwoojong <skekdnwhd@gmail.com>
2024-07-10 10:44:22 +0900

    Feat : Add common response and custom exception to existing logic

--- api-user/src/main/java/com/user/controller/AuthController.java
+++ api-user/src/main/java/com/user/controller/AuthController.java
@@ -2,13 +2,14 @@ package com.user.controller;
 
 import com.user.controller.request.UserRegisterRequest;
 import com.user.service.UserService;
+import com.user.utils.response.ApiResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -19,9 +20,10 @@ public class AuthController {
     private final UserService userService;
 
     @PostMapping("/signup")
-    public ResponseEntity<Void> signup(@RequestBody @Valid UserRegisterRequest request) {
+    @ResponseStatus(HttpStatus.CREATED)
+    public ApiResponse<Void> signup(@RequestBody @Valid UserRegisterRequest request) {
         userService.register(request);
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        return ApiResponse.success();
     }
 
 }
