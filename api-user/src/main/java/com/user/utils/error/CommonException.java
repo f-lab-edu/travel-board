0ea15ac98a506a7e842c34d00c244631a29d3717
Kim woo jong <skekdnwhd@gmail.com>
2024-07-11 23:17:53 +0900

    Fix: Delete log when CommonException occurs

--- api-user/src/main/java/com/user/utils/error/CommonException.java
+++ api-user/src/main/java/com/user/utils/error/CommonException.java
@@ -3,11 +3,11 @@ package com.user.utils.error;
 import lombok.Getter;
 
 @Getter
-public class ApplicationException extends RuntimeException {
+public class CommonException extends RuntimeException {
 
     private final ErrorType errorType;
 
-    public ApplicationException(ErrorType errorType) {
+    public CommonException(ErrorType errorType) {
         super(errorType.getMessage());
         this.errorType = errorType;
     }
