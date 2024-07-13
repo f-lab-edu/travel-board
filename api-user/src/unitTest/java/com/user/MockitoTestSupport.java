6c388c5136ec060b4a3babb9dd73ce1c4def5e16
Kim woo jong <skekdnwhd@gmail.com>
2024-07-14 03:22:05 +0900

    Test: E2E test setup

--- api-user/src/unitTest/java/com/user/MockitoTestSupport.java
+++ api-user/src/unitTest/java/com/user/MockitoTestSupport.java
@@ -2,9 +2,7 @@ package com.user;
 
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("local")
 @ExtendWith(MockitoExtension.class)
 public abstract class MockitoTestSupport {
 
