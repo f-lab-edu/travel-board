0bd2c6adaa1918691682a30ef7916b63fa3fb930
Kim woo jong <skekdnwhd@gmail.com>
2024-07-14 23:54:45 +0900

    Fix: Changed the method of importing basePackage so that it does not import Jpa settings during WebMvcTest

--- api-user/src/main/java/com/user/ApiUserApplication.java
+++ api-user/src/main/java/com/user/ApiUserApplication.java
@@ -2,10 +2,8 @@ package com.user;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
-@ComponentScan(basePackages = {"com.user", "com.storage.config"})
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.user", "com.storage.config"})
 public class ApiUserApplication {
 
     public static void main(String[] args) {
