a82563e78ac6c290871f79f66c034af81a0aed56
Kim woo jong <skekdnwhd@gmail.com>
2024-07-14 23:57:07 +0900

    Test: Remove code unrelated to pr

--- api-user/src/unitTest/java/com/user/MockitoTestSupport.java
+++
@@ -1,9 +0,0 @@
-package com.user;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public abstract class MockitoTestSupport {
-
-}
