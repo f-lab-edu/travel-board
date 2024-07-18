5ca5657088c28a21a8bb519d358926666b945730
Kim woo jong <skekdnwhd@gmail.com>
2024-07-18 21:01:07 +0900

    Test: parameterized test for invalid requests when signup

--- api-user/src/unitTest/java/com/user/controller/AuthControllerTest.java
+++ api-user/src/unitTest/java/com/user/controller/AuthControllerTest.java
@@ -4,9 +4,17 @@ import com.user.ControllerTestSupport;
 import com.user.controller.request.UserRegisterRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.springframework.http.MediaType;
+
+import java.util.stream.Stream;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 class AuthControllerTest extends ControllerTestSupport {
@@ -20,9 +28,22 @@ class AuthControllerTest extends ControllerTestSupport {
         // when & then
         mockMvc.perform(post("/auth/signup")
                         .contentType(APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request))
-                )
+                        .content(objectMapper.writeValueAsString(request)))
                 .andExpect(status().isCreated());
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideInvalidUserRegisterRequests")
+    @DisplayName("유효하지 않은 요청으로 회원가입이 실패하면 400 Bad Request 응답이 반환되어야 한다")
+    void signupValidationFailure(UserRegisterRequest request) throws Exception {
+        // when & then
+        mockMvc.perform(post("/auth/signup")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request)))
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.code").value(400))
+                .andExpect(jsonPath("$.message").value("Request validation failed"));
     }
 
     private UserRegisterRequest getUserRegisterRequest() {
@@ -32,6 +53,29 @@ class AuthControllerTest extends ControllerTestSupport {
                 "nickname",
                 "https://profileImageUrl.png",
                 "bio"
+        );
+    }
+
+    private static Stream<Arguments> provideInvalidUserRegisterRequests() {
+        return Stream.of(
+                // Empty email
+                Arguments.of(new UserRegisterRequest("", "password", "nickname", "https://profileImageUrl.png", "bio")),
+                // Invalid email format
+                Arguments.of(new UserRegisterRequest("invalidemail", "password", "nickname", "https://profileImageUrl.png", "bio")),
+                // Password too short
+                Arguments.of(new UserRegisterRequest("valid@gmail.com", "short", "nickname", "https://profileImageUrl.png", "bio")),
+                // Password too long
+                Arguments.of(new UserRegisterRequest("valid@gmail.com", "longlonglonglonglonglong", "nickname", "https://profileImageUrl.png", "bio")),
+                // Invalid Password format
+                Arguments.of(new UserRegisterRequest("valid@gmail.com", "password)(**&&", "nickname", "https://profileImageUrl.png", "bio")),
+                // Empty nickname
+                Arguments.of(new UserRegisterRequest("valid@gmail.com", "password", "", "https://profileImageUrl.png", "bio")),
+                // Invalid nickname format
+                Arguments.of(new UserRegisterRequest("valid@gmail.com", "password", "nick name", "https://profileImageUrl.png", "bio")),
+                // Invalid URL format
+                Arguments.of(new UserRegisterRequest("valid@gmail.com", "password", "nickname", "invalid_url", "bio")),
+                // Bio too long
+                Arguments.of(new UserRegisterRequest("valid@gmail.com", "password!", "nickname", "https://profileImageUrl.png", "가".repeat(101)))
         );
     }
 }
