3eea89a559e84c1e147d4e0ef3c21433727269f1
kimwoojong <skekdnwhd@gmail.com>
2024-07-22 16:48:40 +0900

    Test: change UserRegisterRequest creation to be created in Fixture class

---
+++ api-user/src/test/java/com/user/support/fixture/dto/request/UserRegisterRequestFixtureFactory.java
@@ -0,0 +1,61 @@
+package com.user.support.fixture.dto.request;
+
+import com.user.controller.request.UserRegisterRequest;
+
+import java.util.List;
+
+public class UserRegisterRequestFixtureFactory {
+
+    private static final String DEFAULT_EMAIL = "valid@gmail.com";
+    private static final String DEFAULT_PASSWORD = "password";
+    private static final String DEFAULT_NICKNAME = "nickname";
+    private static final String DEFAULT_PROFILE_IMAGE_URL = "https://travel/profileImageUrl.png";
+    private static final String DEFAULT_BIO = "introduce myself";
+
+    public static UserRegisterRequest create() {
+        return new UserRegisterRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, DEFAULT_NICKNAME, DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO);
+    }
+
+    public static List<UserRegisterRequest> getInvalidEmailRequests() {
+        return List.of(
+                new UserRegisterRequest(null, DEFAULT_PASSWORD, DEFAULT_NICKNAME, DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO),
+                new UserRegisterRequest("", DEFAULT_PASSWORD, DEFAULT_NICKNAME, DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO),
+                new UserRegisterRequest("invalid_email", DEFAULT_PASSWORD, DEFAULT_NICKNAME, DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO)
+        );
+    }
+
+    public static List<UserRegisterRequest> getInvalidPasswordRequests() {
+        return List.of(
+                new UserRegisterRequest(DEFAULT_EMAIL, null, DEFAULT_NICKNAME, DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO),
+                new UserRegisterRequest(DEFAULT_EMAIL, "", DEFAULT_NICKNAME, DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO),
+                new UserRegisterRequest(DEFAULT_EMAIL, "short", DEFAULT_NICKNAME, DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO),
+                new UserRegisterRequest(DEFAULT_EMAIL, "longlonglonglonglonglong", DEFAULT_NICKNAME, DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO),
+                new UserRegisterRequest(DEFAULT_EMAIL, "password)(**&&", DEFAULT_NICKNAME, DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO)
+        );
+    }
+
+    public static List<UserRegisterRequest> getInvalidNicknameRequests() {
+        return List.of(
+                new UserRegisterRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, null, DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO),
+                new UserRegisterRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, "", DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO),
+                new UserRegisterRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, "n".repeat(2), DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO),
+                new UserRegisterRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, "nick name", DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO),
+                new UserRegisterRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, "닉네임", DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO),
+                new UserRegisterRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, "n".repeat(21), DEFAULT_PROFILE_IMAGE_URL, DEFAULT_BIO)
+        );
+    }
+
+    public static List<UserRegisterRequest> getInvalidProfileImageUrlRequests() {
+        return List.of(
+                new UserRegisterRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, DEFAULT_NICKNAME, "invalid_url", DEFAULT_BIO),
+                new UserRegisterRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, DEFAULT_NICKNAME, "L".repeat(513), DEFAULT_BIO),
+                new UserRegisterRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, DEFAULT_NICKNAME, "http", DEFAULT_BIO)
+        );
+    }
+
+    public static List<UserRegisterRequest> getInvalidBioRequests() {
+        return List.of(
+                new UserRegisterRequest(DEFAULT_EMAIL, DEFAULT_PASSWORD, DEFAULT_NICKNAME, DEFAULT_PROFILE_IMAGE_URL, "가".repeat(101))
+        );
+    }
+}
--- api-user/src/unitTest/java/com/user/controller/AuthControllerTest.java
+++ api-user/src/unitTest/java/com/user/controller/AuthControllerTest.java
@@ -3,21 +3,21 @@ package com.user.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.user.controller.request.UserRegisterRequest;
 import com.user.service.UserService;
+import com.user.support.fixture.dto.request.UserRegisterRequestFixtureFactory;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.DynamicTest;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.Arguments;
-import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.api.TestFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.stream.Stream;
 
 import static com.user.utils.error.ErrorType.INVALID_REQUEST;
+import static org.junit.jupiter.api.DynamicTest.dynamicTest;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -38,14 +38,12 @@ class AuthControllerTest {
     @MockBean
     private UserService userService;
 
-    private static final UserRegisterRequest VALID_SIGNUP_REQUEST = new UserRegisterRequest("valid@gmail.com",
-            "password", "nickname", "https://profileImageUrl.png", "bio");
-
     @Test
     @DisplayName("회원가입이 성공하면 201 Created 응답이 반환되어야 한다")
     void signupSuccess() throws Exception {
         // given
-        String json = objectMapper.writeValueAsString(VALID_SIGNUP_REQUEST);
+        UserRegisterRequest request = UserRegisterRequestFixtureFactory.create();
+        String json = objectMapper.writeValueAsString(request);
 
         // when & then
         mockMvc.perform(post("/auth/signup")
@@ -54,32 +52,88 @@ class AuthControllerTest {
                 .andExpect(status().isCreated());
     }
 
-    @ParameterizedTest(name = "{1} 유효성 검증 실패")
-    @MethodSource("provideInvalidUserRegisterRequests")
-    @DisplayName("유효하지 않은 요청으로 회원가입이 실패하면 400 Bad Request 응답이 반환되어야 한다")
-    void signupValidationFailure(UserRegisterRequest request, String validationField) throws Exception {
-        // when & then
-        mockMvc.perform(post("/auth/signup")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(objectMapper.writeValueAsString(request)))
-                .andExpect(status().isBadRequest())
-                .andExpect(jsonPath("$.code").value(400))
-                .andExpect(jsonPath("$.message").value(INVALID_REQUEST.getMessage()))
-                .andExpect(jsonPath(String.format("$.validations.%s", validationField)).isNotEmpty());
+    @TestFactory
+    @DisplayName("회원가입 시 이메일 유효성 검증 실패하면 400 Bad Request 응답이 반환되어야 한다")
+    Stream<DynamicTest> emailValidationFailure() {
+        return UserRegisterRequestFixtureFactory.getInvalidEmailRequests().stream()
+                .map(request -> dynamicTest(
+                                String.format("이메일에 %s를 입력하면 유효성 검증이 실패한다", request.email()),
+                                () -> mockMvc.perform(post("/auth/signup")
+                                                .contentType(APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(request)))
+                                        .andExpect(status().isBadRequest())
+                                        .andExpect(jsonPath("$.code").value(400))
+                                        .andExpect(jsonPath("$.message").value(INVALID_REQUEST.getMessage()))
+                                        .andExpect(jsonPath("$.validations.email").isNotEmpty())
+                        )
+                );
     }
 
-    private static Stream<Arguments> provideInvalidUserRegisterRequests() {
-        return Stream.of(
-                Arguments.of(new UserRegisterRequest("", VALID_SIGNUP_REQUEST.password(), VALID_SIGNUP_REQUEST.nickname(), VALID_SIGNUP_REQUEST.profileImageUrl(), VALID_SIGNUP_REQUEST.bio()), "email"),
-                Arguments.of(new UserRegisterRequest("invalid_email", VALID_SIGNUP_REQUEST.password(), VALID_SIGNUP_REQUEST.nickname(), VALID_SIGNUP_REQUEST.profileImageUrl(), VALID_SIGNUP_REQUEST.bio()), "email"),
-                Arguments.of(new UserRegisterRequest(VALID_SIGNUP_REQUEST.email(), "", VALID_SIGNUP_REQUEST.nickname(), VALID_SIGNUP_REQUEST.profileImageUrl(), VALID_SIGNUP_REQUEST.bio()), "password"),
-                Arguments.of(new UserRegisterRequest(VALID_SIGNUP_REQUEST.email(), "short", VALID_SIGNUP_REQUEST.nickname(), VALID_SIGNUP_REQUEST.profileImageUrl(), VALID_SIGNUP_REQUEST.bio()), "password"),
-                Arguments.of(new UserRegisterRequest(VALID_SIGNUP_REQUEST.email(), "longlonglonglonglonglong", VALID_SIGNUP_REQUEST.nickname(), VALID_SIGNUP_REQUEST.profileImageUrl(), VALID_SIGNUP_REQUEST.bio()), "password"),
-                Arguments.of(new UserRegisterRequest(VALID_SIGNUP_REQUEST.email(), "password)(**&&", VALID_SIGNUP_REQUEST.nickname(), VALID_SIGNUP_REQUEST.profileImageUrl(), VALID_SIGNUP_REQUEST.bio()), "password"),
-                Arguments.of(new UserRegisterRequest(VALID_SIGNUP_REQUEST.email(), VALID_SIGNUP_REQUEST.password(), "", VALID_SIGNUP_REQUEST.profileImageUrl(), VALID_SIGNUP_REQUEST.bio()), "nickname"),
-                Arguments.of(new UserRegisterRequest(VALID_SIGNUP_REQUEST.email(), VALID_SIGNUP_REQUEST.password(), "nick name", VALID_SIGNUP_REQUEST.profileImageUrl(), VALID_SIGNUP_REQUEST.bio()), "nickname"),
-                Arguments.of(new UserRegisterRequest(VALID_SIGNUP_REQUEST.email(), VALID_SIGNUP_REQUEST.password(), VALID_SIGNUP_REQUEST.nickname(), "invalid_url", VALID_SIGNUP_REQUEST.bio()), "profileImageUrl"),
-                Arguments.of(new UserRegisterRequest(VALID_SIGNUP_REQUEST.email(), VALID_SIGNUP_REQUEST.password(), VALID_SIGNUP_REQUEST.nickname(), VALID_SIGNUP_REQUEST.profileImageUrl(), "가".repeat(101)), "bio")
-        );
+    @TestFactory
+    @DisplayName("회원가입 시 비밀번호 유효성 검증 실패하면 400 Bad Request 응답이 반환되어야 한다")
+    Stream<DynamicTest> passwordValidationFailure() {
+        return UserRegisterRequestFixtureFactory.getInvalidPasswordRequests().stream()
+                .map(request -> dynamicTest(
+                                String.format("비밀번호에 %s를 입력하면 유효성 검증이 실패한다", request.password()),
+                                () -> mockMvc.perform(post("/auth/signup")
+                                                .contentType(APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(request)))
+                                        .andExpect(status().isBadRequest())
+                                        .andExpect(jsonPath("$.code").value(400))
+                                        .andExpect(jsonPath("$.message").value(INVALID_REQUEST.getMessage()))
+                                        .andExpect(jsonPath("$.validations.password").isNotEmpty())
+                        )
+                );
+    }
+
+    @TestFactory
+    @DisplayName("회원가입 시 닉네임 유효성 검증 실패하면 400 Bad Request 응답이 반환되어야 한다")
+    Stream<DynamicTest> nicknameValidationFailure() {
+        return UserRegisterRequestFixtureFactory.getInvalidNicknameRequests().stream()
+                .map(request -> dynamicTest(
+                                String.format("닉네임에 %s를 입력하면 유효성 검증이 실패한다", request.nickname()),
+                                () -> mockMvc.perform(post("/auth/signup")
+                                                .contentType(APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(request)))
+                                        .andExpect(status().isBadRequest())
+                                        .andExpect(jsonPath("$.code").value(400))
+                                        .andExpect(jsonPath("$.message").value(INVALID_REQUEST.getMessage()))
+                                        .andExpect(jsonPath("$.validations.nickname").isNotEmpty())
+                        )
+                );
+    }
+
+    @TestFactory
+    @DisplayName("회원가입 시 프로필 이미지 URL 유효성 검증 실패하면 400 Bad Request 응답이 반환되어야 한다")
+    Stream<DynamicTest> profileImageUrlValidationFailure() {
+        return UserRegisterRequestFixtureFactory.getInvalidProfileImageUrlRequests().stream()
+                .map(request -> dynamicTest(
+                                String.format("프로필 이미지 URL에 %s를 입력하면 유효성 검증이 실패한다", request.profileImageUrl()),
+                                () -> mockMvc.perform(post("/auth/signup")
+                                                .contentType(APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(request)))
+                                        .andExpect(status().isBadRequest())
+                                        .andExpect(jsonPath("$.code").value(400))
+                                        .andExpect(jsonPath("$.message").value(INVALID_REQUEST.getMessage()))
+                                        .andExpect(jsonPath("$.validations.profileImageUrl").isNotEmpty())
+                        )
+                );
+    }
+
+    @TestFactory
+    @DisplayName("회원가입 시 소개글 유효성 검증 실패하면 400 Bad Request 응답이 반환되어야 한다")
+    Stream<DynamicTest> bioValidationFailure() {
+        return UserRegisterRequestFixtureFactory.getInvalidBioRequests().stream()
+                .map(request -> dynamicTest(
+                                String.format("소개에 %s를 입력하면 유효성 검증이 실패한다", request.bio()),
+                                () -> mockMvc.perform(post("/auth/signup")
+                                                .contentType(APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(request)))
+                                        .andExpect(status().isBadRequest())
+                                        .andExpect(jsonPath("$.code").value(400))
+                                        .andExpect(jsonPath("$.message").value(INVALID_REQUEST.getMessage()))
+                                        .andExpect(jsonPath("$.validations.bio").isNotEmpty())
+                        )
+                );
     }
 }
