167d4f576f358e21a103d86428b915eda1aa98ac
kimwoojong <skekdnwhd@gmail.com>
2024-07-26 17:17:47 +0900

    Fix: remove TokenResponse

--- api-user/src/main/java/com/user/utils/token/TokenResponse.java
+++
@@ -1,9 +0,0 @@
-package com.user.utils.token;
-
-
-public record TokenResponse(String accessToken, String refreshToken) {
-
-    public static TokenResponse of(String accessToken, String refreshToken) {
-        return new TokenResponse(accessToken, refreshToken);
-    }
-}
