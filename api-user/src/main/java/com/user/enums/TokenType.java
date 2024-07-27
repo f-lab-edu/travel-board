c02ce9d48783cce18f07c1fb99a7d08fee8958cf
Kim woo jong <skekdnwhd@gmail.com>
2024-07-28 00:04:57 +0900

    Fix: Injection of token-related properties

--- api-user/src/main/java/com/user/enums/TokenType.java
+++ api-user/src/main/java/com/user/enums/TokenType.java
@@ -1,7 +1,22 @@
 package com.user.enums;
 
+import lombok.Getter;
+
+import javax.crypto.SecretKey;
+
+@Getter
 public enum TokenType {
 
-    ACCESS, REFRESH
+    ACCESS, REFRESH;
 
+    private SecretKey secretKey;
+    private long validityMilliseconds;
+
+    public void setSecretKey(SecretKey secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public void setValidityMilliseconds(long validityMilliseconds) {
+        this.validityMilliseconds = validityMilliseconds;
+    }
 }
