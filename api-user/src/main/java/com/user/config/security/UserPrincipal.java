99932f477b2f3343ce9f2dfcb2914cca258602eb
kimwoojong <skekdnwhd@gmail.com>
2024-07-29 14:29:45 +0900

    Fix: change the imported User

--- api-user/src/main/java/com/user/config/security/UserPrincipal.java
+++ api-user/src/main/java/com/user/config/security/UserPrincipal.java
@@ -1,18 +1,18 @@
 package com.user.config.security;
 
+import com.storage.entity.User;
 import lombok.Getter;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 
 import java.util.List;
 
 @Getter
-public class UserPrincipal extends User {
+public class UserPrincipal extends org.springframework.security.core.userdetails.User {
 
-    private final com.storage.entity.User user;
+    private final User user;
     private static final String ROLE_USER = "ROLE_USER";
 
-    public UserPrincipal(com.storage.entity.User user) {
+    public UserPrincipal(User user) {
         super(user.getAccount().getEmail(), user.getAccount().getPassword(), List.of(new SimpleGrantedAuthority(ROLE_USER)));
         this.user = user;
     }
