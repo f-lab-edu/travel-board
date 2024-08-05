f25acd0e14d24aba3b60f7b0dc3749c23116b6eb
Kim woo jong <skekdnwhd@gmail.com>
2024-08-06 03:55:17 +0900

    Test: add login and token reissue e2e test cases

--- api-user/src/test/java/com/user/support/fixture/entity/AccountFixtureFactory.java
+++ api-user/src/test/java/com/user/support/fixture/entity/AccountFixtureFactory.java
@@ -1,6 +1,7 @@
 package com.user.support.fixture.entity;
 
 import com.storage.entity.Account;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class AccountFixtureFactory {
 
@@ -8,6 +9,13 @@ public class AccountFixtureFactory {
         return Account.builder()
                 .email("valid@email.com")
                 .password("encodedPassword")
+                .build();
+    }
+
+    public static Account create(PasswordEncoder passwordEncoder, String password) {
+        return Account.builder()
+                .email("valid@email.com")
+                .password(passwordEncoder.encode(password))
                 .build();
     }
 }
