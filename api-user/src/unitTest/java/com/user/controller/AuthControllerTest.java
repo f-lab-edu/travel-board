8b14ad63eae3b5b99d29ad1127a6447efb921357
Kim woo jong <skekdnwhd@gmail.com>
2024-08-06 02:17:23 +0900

    Test: add login, reissueAccessToken controller test cases

--- api-user/src/unitTest/java/com/user/controller/AuthControllerTest.java
+++ api-user/src/unitTest/java/com/user/controller/AuthControllerTest.java
@@ -2,8 +2,14 @@ package com.user.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.user.config.security.SecurityConfig;
+import com.user.dto.request.AccessTokenReissueRequest;
+import com.user.dto.request.LoginRequest;
 import com.user.dto.request.UserRegisterRequest;
+import com.user.dto.response.TokenResponse;
+import com.user.enums.ErrorType;
 import com.user.service.AuthService;
+import com.user.support.fixture.dto.request.AccessTokenReissueRequestFixtureFactory;
+import com.user.support.fixture.dto.request.LoginRequestFixtureFactory;
 import com.user.support.fixture.dto.request.UserRegisterRequestFixtureFactory;
 import com.user.utils.error.CommonException;
 import com.user.utils.token.JwtTokenProvider;
@@ -22,9 +28,12 @@ import java.util.stream.Stream;
 import static com.user.enums.ErrorType.DEFAULT_ERROR;
 import static com.user.enums.ErrorType.DUPLICATED_EMAIL;
 import static com.user.enums.ErrorType.INVALID_REQUEST;
+import static com.user.enums.ErrorType.UNAUTHORIZED_TOKEN;
 import static org.junit.jupiter.api.DynamicTest.dynamicTest;
+import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.doThrow;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -177,5 +186,159 @@ class AuthControllerTest {
                                         .andExpect(jsonPath("$.validations.bio").isNotEmpty())
                         )
                 );
+    }
+
+    @Test
+    @DisplayName("로그인 성공 시 200 OK 응답이 반환되어야 한다")
+    void loginSuccess() throws Exception {
+        // given
+        LoginRequest request = LoginRequestFixtureFactory.create();
+        String json = objectMapper.writeValueAsString(request);
+        TokenResponse expectedResponse = new TokenResponse("accessToken", "refreshToken");
+        given(authService.login(request)).willReturn(expectedResponse);
+
+        // when && then
+        mockMvc.perform(post("/auth/login")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").value("accessToken"))
+                .andExpect(jsonPath("$.refreshToken").value("refreshToken"));
+    }
+
+    @Test
+    @DisplayName("로그인 실패 시 401 Unauthorized 응답이 반환되어야 한다")
+    void loginFailure() throws Exception {
+        // given
+        LoginRequest request = LoginRequestFixtureFactory.create();
+        String json = objectMapper.writeValueAsString(request);
+        doThrow(new CommonException(ErrorType.LOGIN_FAIL)).when(authService).login(request);
+
+        // when && then
+        mockMvc.perform(post("/auth/login")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.code").value(401))
+                .andExpect(jsonPath("$.message").value(ErrorType.LOGIN_FAIL.getMessage()));
+    }
+
+    @TestFactory
+    @DisplayName("로그인 시 이메일 유효성 검증 실패하면 400 Bad Request 응답이 반환되어야 한다")
+    Stream<DynamicTest> loginEmailValidationFailure() {
+        return LoginRequestFixtureFactory.getInvalidEmailRequests().stream()
+                .map(request -> dynamicTest(
+                                String.format("이메일에 %s를 입력하면 유효성 검증이 실패한다", request.email()),
+                                () -> mockMvc.perform(post("/auth/login")
+                                                .contentType(APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(request)))
+                                        .andExpect(status().isBadRequest())
+                                        .andExpect(jsonPath("$.code").value(400))
+                                        .andExpect(jsonPath("$.message").value(INVALID_REQUEST.getMessage()))
+                                        .andExpect(jsonPath("$.validations.email").isNotEmpty())
+                        )
+                );
+    }
+
+    @TestFactory
+    @DisplayName("로그인 시 패스워드 유효성 검증 실패하면 400 Bad Request 응답이 반환되어야 한다")
+    Stream<DynamicTest> loginPasswordValidationFailure() {
+        return LoginRequestFixtureFactory.getInvalidPasswordRequests().stream()
+                .map(request -> dynamicTest(
+                                String.format("패스워드에 %s를 입력하면 유효성 검증이 실패한다", request.password()),
+                                () -> mockMvc.perform(post("/auth/login")
+                                                .contentType(APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(request)))
+                                        .andExpect(status().isBadRequest())
+                                        .andExpect(jsonPath("$.code").value(400))
+                                        .andExpect(jsonPath("$.message").value(INVALID_REQUEST.getMessage()))
+                                        .andExpect(jsonPath("$.validations.password").isNotEmpty())
+                        )
+                );
+    }
+
+    @Test
+    @DisplayName("로그인 시 예상할 수 없는 예외가 발생하면 500 Internal Server Error 응답이 반환되어야 한다")
+    void loginFailureWhenUserServiceThrowException() throws Exception {
+        // given
+        LoginRequest request = LoginRequestFixtureFactory.create();
+        String json = objectMapper.writeValueAsString(request);
+        doThrow(new RuntimeException()).when(authService).login(request);
+
+        // when & then
+        mockMvc.perform(post("/auth/login")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.code").value(500))
+                .andExpect(jsonPath("$.message").value(DEFAULT_ERROR.getMessage()));
+    }
+
+    @Test
+    @DisplayName("엑세스 토큰 재발급 성공 시 200 OK 응답이 반환되어야 한다")
+    void reissueAccessTokenSuccess() throws Exception {
+        // given
+        AccessTokenReissueRequest request = AccessTokenReissueRequestFixtureFactory.createMockRefreshToken();
+        String json = objectMapper.writeValueAsString(request);
+        String expectedAccessToken = "newAccessToken";
+        given(authService.reissueAccessToken(request.refreshToken())).willReturn(expectedAccessToken);
+
+        // when && then
+        mockMvc.perform(patch("/auth/access-token")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.accessToken").value(expectedAccessToken));
+    }
+
+    @Test
+    @DisplayName("엑세스 토큰 재발급 실패 시 401 Unauthorized 응답이 반환되어야 한다")
+    void reissueAccessTokenFailure() throws Exception {
+        // given
+        AccessTokenReissueRequest request = AccessTokenReissueRequestFixtureFactory.createMockRefreshToken();
+        String json = objectMapper.writeValueAsString(request);
+        doThrow(new CommonException(UNAUTHORIZED_TOKEN)).when(authService).reissueAccessToken(request.refreshToken());
+
+        // when && then
+        mockMvc.perform(patch("/auth/access-token")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isUnauthorized())
+                .andExpect(jsonPath("$.code").value(401))
+                .andExpect(jsonPath("$.message").value(UNAUTHORIZED_TOKEN.getMessage()));
+    }
+
+    @TestFactory
+    @DisplayName("엑세스 토큰 재발급 시 리프레시 토큰 유효성 검증 실패하면 400 Bad Request 응답이 반환되어야 한다")
+    Stream<DynamicTest> reissueAccessTokenValidationFailure() {
+        return AccessTokenReissueRequestFixtureFactory.getInvalidRefreshTokenRequests().stream()
+                .map(request -> dynamicTest(
+                                String.format("리프레시 토큰에 %s를 입력하면 유효성 검증이 실패한다", request.refreshToken()),
+                                () -> mockMvc.perform(patch("/auth/access-token")
+                                                .contentType(APPLICATION_JSON)
+                                                .content(objectMapper.writeValueAsString(request)))
+                                        .andExpect(status().isBadRequest())
+                                        .andExpect(jsonPath("$.code").value(400))
+                                        .andExpect(jsonPath("$.message").value(INVALID_REQUEST.getMessage()))
+                                        .andExpect(jsonPath("$.validations.refreshToken").isNotEmpty())
+                        )
+                );
+    }
+
+    @Test
+    @DisplayName("엑세스 토큰 재발급 시 예상할 수 없는 예외가 발생하면 500 Internal Server Error 응답이 반환되어야 한다")
+    void reissueAccessTokenFailureWhenUserServiceThrowException() throws Exception {
+        // given
+        AccessTokenReissueRequest request = AccessTokenReissueRequestFixtureFactory.createMockRefreshToken();
+        String json = objectMapper.writeValueAsString(request);
+        doThrow(new RuntimeException()).when(authService).reissueAccessToken(request.refreshToken());
+
+        // when & then
+        mockMvc.perform(patch("/auth/access-token")
+                        .contentType(APPLICATION_JSON)
+                        .content(json))
+                .andExpect(status().isInternalServerError())
+                .andExpect(jsonPath("$.code").value(500))
+                .andExpect(jsonPath("$.message").value(DEFAULT_ERROR.getMessage()));
     }
 }
