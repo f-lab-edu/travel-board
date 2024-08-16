7992fa4815f06200a10134db49f3ad406cc9b7ba
Kim woo jong <skekdnwhd@gmail.com>
2024-08-17 01:33:23 +0900

    Refactor: move CurrentUserArgumentResolver

--- api-user/src/main/java/com/user/config/WebConfig.java
+++ api-user/src/main/java/com/user/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.user.config;
 
+import com.user.config.security.CurrentUserArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
