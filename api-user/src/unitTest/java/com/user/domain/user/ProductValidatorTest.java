6bc3633d47bd6756bdf732aa29eebda5fac117d4
Kim woo jong <skekdnwhd@gmail.com>
2024-08-19 01:01:44 +0900

    Test: Post register Service Layer unit tests

---
+++ api-user/src/unitTest/java/com/user/domain/user/ProductValidatorTest.java
@@ -0,0 +1,67 @@
+package com.user.domain.user;
+
+import com.storage.entity.Product;
+import com.storage.entity.ProductLevel;
+import com.storage.entity.User;
+import com.user.support.fixture.entity.AccountFixtureFactory;
+import com.user.support.fixture.entity.ProductFixtureFactory;
+import com.user.support.fixture.entity.UserFixtureFactory;
+import com.user.utils.error.CommonException;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static com.user.enums.ErrorType.PRODUCT_PREMIUM_REQUIRED;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+
+@ExtendWith(MockitoExtension.class)
+class ProductValidatorTest {
+
+    @InjectMocks
+    private ProductValidator productValidator;
+
+    @Test
+    @DisplayName("프리미엄 상품이면 예외가 발생하지 않아야 한다")
+    void validateDoesNotThrowExceptionForPremiumProduct() {
+        // given
+        User user = UserFixtureFactory.create(AccountFixtureFactory.create());
+        Product product = ProductFixtureFactory.createWith(user, ProductLevel.PREMIUM);
+        LocalDateTime now = product.getEndAt().minusDays(1L);
+
+        // when && then
+        assertDoesNotThrow(() -> productValidator.validate(product, now));
+    }
+
+    @Test
+    @DisplayName("프리미엄 상품이 아니면 예외가 발생한다.")
+    void validateThrowsExceptionForNotPremiumProduct() {
+        // given
+        User user = UserFixtureFactory.create(AccountFixtureFactory.create());
+        Product product = ProductFixtureFactory.createWith(user, ProductLevel.BASIC);
+        LocalDateTime now = product.getEndAt().minusDays(1L);
+
+        // when && then
+        assertThatThrownBy(() -> productValidator.validate(product, now))
+                .isInstanceOf(CommonException.class)
+                .hasMessage(PRODUCT_PREMIUM_REQUIRED.getMessage());
+    }
+
+    @Test
+    @DisplayName("만료된 상품이면 예외가 발생한다.")
+    void validateThrowsExceptionForExpiredProduct() {
+        // given
+        User user = UserFixtureFactory.create(AccountFixtureFactory.create());
+        Product product = ProductFixtureFactory.createWith(user, ProductLevel.PREMIUM);
+        LocalDateTime now = product.getEndAt().plusDays(1L);
+
+        // when && then
+        assertThatThrownBy(() -> productValidator.validate(product, now))
+                .isInstanceOf(CommonException.class)
+                .hasMessage(PRODUCT_PREMIUM_REQUIRED.getMessage());
+    }
+}
