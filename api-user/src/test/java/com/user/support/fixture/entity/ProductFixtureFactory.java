6c9a4058aa40157f16fb6ef792761ca7fd418da4
Kim woo jong <skekdnwhd@gmail.com>
2024-08-19 21:25:39 +0900

    Test: add Post register api e2e test cases

--- api-user/src/test/java/com/user/support/fixture/entity/ProductFixtureFactory.java
+++ api-user/src/test/java/com/user/support/fixture/entity/ProductFixtureFactory.java
@@ -16,4 +16,13 @@ public class ProductFixtureFactory {
                 .endAt(LocalDateTime.of(2024, 12, 31, 23, 59))
                 .build();
     }
+
+    public static Product createWith(User user, ProductLevel productLevel, LocalDateTime endAt) {
+        return Product.builder()
+                .user(user)
+                .level(productLevel)
+                .startAt(LocalDateTime.of(2024, 1, 1, 0, 0))
+                .endAt(endAt)
+                .build();
+    }
 }
