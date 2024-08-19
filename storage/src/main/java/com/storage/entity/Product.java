bbecd35aee696efed4cafe471fb6e32cb4e15139
kimwoojong <skekdnwhd@gmail.com>
2024-08-19 17:55:40 +0900

    Fix: Change to lazy loading of relationships

--- storage/src/main/java/com/storage/entity/Product.java
+++ storage/src/main/java/com/storage/entity/Product.java
@@ -1,14 +1,6 @@
 package com.storage.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.ForeignKey;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -16,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
+import static jakarta.persistence.FetchType.LAZY;
 import static jakarta.persistence.GenerationType.IDENTITY;
 
 @Entity
@@ -27,7 +20,7 @@ public class Product extends BaseEntity {
     @GeneratedValue(strategy = IDENTITY)
     private Long id;
 
-    @OneToOne
+    @OneToOne(fetch = LAZY)
     @JoinColumn(name = "user_id", foreignKey = @ForeignKey(name = "FK_product_user_id"))
     private User user;
 
