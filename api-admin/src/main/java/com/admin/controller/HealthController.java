0e981a22108f62951dc929c889689254fa7123b8
Kim woo jong <skekdnwhd@gmail.com>
2024-08-28 00:09:13 +0900

    Chore: Add ci for api-admin

---
+++ api-admin/src/main/java/com/admin/controller/HealthController.java
@@ -0,0 +1,14 @@
+package com.admin.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+
+    @GetMapping("/health")
+    public ResponseEntity<Void> health() {
+        return ResponseEntity.ok().build();
+    }
+}
